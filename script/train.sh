17e42303276113325a95e8ada636e6d2649055b0
leowangzi <leowangzi@163.com>
2019-11-15 15:11:03 +0800

    fix mobilenet bugs

--- script/train.sh
+++ script/train.sh
@@ -2,4 +2,4 @@
 set -e
 cd ..
 
-CUDA_VISIBLE_DEVICES=2  python trainval_net.py --dataset pascal_voc --net res101 --bs 2 --nw 8 --lr 1e-3 --lr_decay_step 5 --epochs 20 --cuda --lighthead
+CUDA_VISIBLE_DEVICES=2  python trainval_net.py --dataset pascal_voc --net mobilenet --bs 2 --nw 8 --lr 1e-3 --lr_decay_step 5 --epochs 20 --cuda --lighthead
