2135707d63e519b517ec7690a6383a0a51083430
leowangzi <leowangzi@163.com>
2019-11-21 14:08:36 +0800

    clean and set layer 4 stride is 1

--- lib/model/faster_rcnn/resnet.py
+++ lib/model/faster_rcnn/resnet.py
@@ -32,95 +32,6 @@ model_urls = {
 }
 
 
-# def conv3x3(in_planes, out_planes, stride=1):
-#     "3x3 convolution with padding"
-#     return nn.Conv2d(in_planes,
-#                      out_planes,
-#                      kernel_size=3,
-#                      stride=stride,
-#                      padding=1,
-#                      bias=False)
-
-
-# class BasicBlock(nn.Module):
-#     expansion = 1
-
-#     def __init__(self, inplanes, planes, stride=1, downsample=None):
-#         super(BasicBlock, self).__init__()
-#         self.conv1 = conv3x3(inplanes, planes, stride)
-#         self.bn1 = nn.BatchNorm2d(planes)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.conv2 = conv3x3(planes, planes)
-#         self.bn2 = nn.BatchNorm2d(planes)
-#         self.downsample = downsample
-#         self.stride = stride
-
-#     def forward(self, x):
-#         residual = x
-
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu(out)
-
-#         out = self.conv2(out)
-#         out = self.bn2(out)
-
-#         if self.downsample is not None:
-#             residual = self.downsample(x)
-
-#         out += residual
-#         out = self.relu(out)
-
-#         return out
-
-# class Bottleneck(nn.Module):
-#     expansion = 4
-
-#     def __init__(self, inplanes, planes, stride=1, downsample=None):
-#         super(Bottleneck, self).__init__()
-#         self.conv1 = nn.Conv2d(inplanes,
-#                                planes,
-#                                kernel_size=1,
-#                                stride=stride,
-#                                bias=False)  # change
-#         self.bn1 = nn.BatchNorm2d(planes)
-#         self.conv2 = nn.Conv2d(
-#             planes,
-#             planes,
-#             kernel_size=3,
-#             stride=1,  # change
-#             padding=1,
-#             bias=False)
-#         self.bn2 = nn.BatchNorm2d(planes)
-#         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-#         self.bn3 = nn.BatchNorm2d(planes * 4)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.downsample = downsample
-#         self.stride = stride
-
-#     def forward(self, x):
-#         residual = x
-
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu(out)
-
-#         out = self.conv2(out)
-#         out = self.bn2(out)
-#         out = self.relu(out)
-
-#         out = self.conv3(out)
-#         out = self.bn3(out)
-
-#         if self.downsample is not None:
-#             residual = self.downsample(x)
-
-#         out += residual
-#         out = self.relu(out)
-
-#         return out
-
-
 class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=1000):
         self.inplanes = 64
@@ -140,9 +51,9 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        # self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         # it is slightly better whereas slower to set stride = 1
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
+        # self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
         self.avgpool = nn.AvgPool2d(7)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -261,7 +172,7 @@ class resnet(_fasterRCNN):
         self.dout_base_model = 1024
         if self.lighthead:
             self.dout_lh_base_model = 2048
-        # self.num_layers = num_layers
+        self.num_layers = num_layers
 
         _fasterRCNN.__init__(self,
                              classes,
@@ -272,14 +183,14 @@ class resnet(_fasterRCNN):
     def _init_modules(self):
         resnet = resnet101()
 
-        # if self.num_layers == 18:
-        #     resnet = resnet18()
-        # if self.num_layers == 34:
-        #     resnet = resnet34()
-        # if self.num_layers == 50:
-        #     resnet = resnet50()
-        # if self.num_layers == 152:
-        #     resnet = resnet152()
+        if self.num_layers == 18:
+            resnet = resnet18()
+        if self.num_layers == 34:
+            resnet = resnet34()
+        if self.num_layers == 50:
+            resnet = resnet50()
+        if self.num_layers == 152:
+            resnet = resnet152()
 
         if self.pretrained == True:
             print("Loading pretrained weights from %s" % (self.model_path))
